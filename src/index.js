127cd0b9355f7a4d8410cc79ef9510652d27610d
CanFISHfly1 <nathanlu_@hotmail.com>
2021-12-13 14:08:24 -0800

    Added GA

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import { ToastManager } from 'ds/hooks/useToast';
 
 import 'assets/styles/index.css';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-X392J39GCK');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+
 ReactDOM.render(
 	<ThemeProvider>
 		<ToastManager>
