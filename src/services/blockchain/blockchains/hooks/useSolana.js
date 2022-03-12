3915a3343c11d2c2681abcf8430252b3b08b9448
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-03-11 19:29:52 -0800

    meow

--- src/services/blockchain/blockchains/hooks/useSolana.js
+++ src/services/blockchain/blockchains/hooks/useSolana.js
@@ -58,9 +58,9 @@ export const useSolana = () => {
                 cacheHash
             });
 
-            handleDeploymentSuccess(id, 'candymachineaddresHERE');
+            //handleDeploymentSuccess(id, 'candymachineaddresHERE');
 
-            // console.log(res);
+            console.log(res);
         }
         catch (err) {
             console.error(err);
