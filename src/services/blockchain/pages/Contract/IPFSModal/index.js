4bb6dfbaa7d168a61b97afd7e10639af4c026a90
Nathan <nathanlu_@hotmail.com>
2022-03-26 18:18:13 -0700

    IPFS modal skip past preview if user already subscribed

--- src/services/blockchain/pages/Contract/IPFSModal/index.js
+++ src/services/blockchain/pages/Contract/IPFSModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Box, Button, IconButton, Stack } from 'ds/components';
 import { Stepper, Step, StepLabel, StepContent } from '@mui/material';
 import StepWizard from 'react-step-wizard';
@@ -11,6 +11,14 @@ import Payment from './Payment';
 import Upload from './Upload';
 
 const IPFSModal = ({ contract, isModalOpen, setIsModalOpen, id }) => {
+	const [initialStep, setInitialStep] = useState(1);
+
+	useEffect(() => {
+		// If contract is subscribed, skip to next step
+		if(contract.isSubscribed) { 
+			setInitialStep(4)
+		}
+	}, [contract])
 
 	return (
 		<Modal
@@ -29,7 +37,7 @@ const IPFSModal = ({ contract, isModalOpen, setIsModalOpen, id }) => {
 					</IconButton>
 				</Stack>
 
-				<StepWizard transitions={{}}>
+				<StepWizard initialStep={initialStep} transitions={{}}>
 					<Preview />
 					{/* user imports their own link */}
 					<ImportLink id={id} setIsModalOpen={setIsModalOpen}  />
